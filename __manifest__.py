1715c2145f25b7743d1b4dac655ff344ba35c908
HafsanaCA <hafsanaca@gmail.com>
2025-05-23 16:51:11 +0530

    Initial Commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Website Decimal Quantity',
+    'version': '1.0',
+    'summary': 'When the user clicks on the + or - button to adjust quantity, It should add or deduct .1 value from it.',
+    'author': 'Hafsana CA',
+    'depends': ['base','website'],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': False,
+}
