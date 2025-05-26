0a24a684f72854122e70559c37b19bf8dd40e191
HafsanaCA <hafsanaca@gmail.com>
2025-05-26 17:31:25 +0530

    Minor Update

--- __manifest__.py
+++ __manifest__.py
@@ -3,10 +3,14 @@
     'version': '1.0',
     'summary': 'When the user clicks on the + or - button to adjust quantity, It should add or deduct .1 value from it.',
     'author': 'Hafsana CA',
-    'depends': ['base','website'],
+    'depends': ['base','website','sale','product'],
     'data': [
-
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'website_decimal_quantity/static/src/js/website_sale_decimal.js',
+        ],
+    },
     'installable': True,
     'application': False,
 }
